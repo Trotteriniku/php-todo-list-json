9cf0fdcfc73f22bb08581a5b464aeb14c7282880
Trotteriniku <stefanozidda3@gmail.com>
2023-12-01 17:41:59 +0100

    added  json array red with the server.php functions

---
+++ server.php
@@ -0,0 +1,25 @@
+<?php
+
+$filecontent = file_get_contents("todo-list.json");
+
+
+
+
+// decodifico la stringa in un array php
+$list = json_decode($filecontent, true);
+
+// pushiamo l'oggetto task precedentemente preso nella action "addTask"
+if (isset($_POST['task'])) {
+    $newtask = $_POST['task'];
+    array_push($list, $newtask);
+    file_put_contents('todo-list.json', json_encode($list));
+}
+
+
+
+header('Content-Type: application/json');
+
+
+// stampo la lista in json
+echo json_encode($list);
+?>
