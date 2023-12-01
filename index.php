7c12a52f068408be92bd62f88468b92d8ed736b5
Trotteriniku <stefanozidda3@gmail.com>
2023-12-01 17:45:00 +0100

    added script functions + vue

--- index.php
+++ index.php
@@ -23,7 +23,8 @@
                         <div class="col-12">
                             <h1 class="display-1 text-light">Todo-List</h1>
                             <ul class="list-group list-group-flush border border-1 rounded">
-                                <li v-for="(task, index) in todolist" :key="index" class="list-group-item ">{{task}}
+                                <li v-for="(task, index) in todolist" :key="index" class="list-group-item ">
+                                    {{task}}
                                 </li>
                             </ul>
                         </div>
---
+++ js/script.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      apiUrl:'server.php',
+      todolist:[],
+      newTask:"",
+    };
+  },
+  methods: {
+    readList(){
+      axios.get(this.apiUrl).then((response)=> {
+          
+          this.todolist = response.data
+      })
+    },
+    addTask(){
+      if(this.newTask == ""){
+        return;
+      }
+      const data = new FormData();
+      data.append("task", this.newTask);
+      
+      axios.post(this.apiUrl, data).then((response) =>{
+        this.todolist = response.data;
+      })
+      
+    }
+  },
+
+  mounted() {
+    this.readList();
+  },
+}).mount("#app");
